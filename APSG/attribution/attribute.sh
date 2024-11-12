01e2792f8ea495d085ff80057a7a2e2c79cc899d
SEdeepL <153978170+SEdeepL@users.noreply.github.com>
2024-11-12 15:54:23 +0800

    Update attribute.sh

--- APSG/attribution/attribute.sh
+++ APSG/attribution/attribute.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+source_file = ""
 patch_text = ""
 bug_text = ""
 graph_file = ""
 
-python3 action.py patch_text bug_text graph_file
+python3 action.py source_file graph_file
 
 python3 apti_pattern.py patch_text bug_text graph_file
 
