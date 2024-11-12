31968d6a9b9d0475e0dce48acf23aee7cb974409
SEdeepL <153978170+SEdeepL@users.noreply.github.com>
2024-11-12 14:20:01 +0800

    Create attribute.sh

---
+++ APSG/attribution/attribute.sh
@@ -0,0 +1,17 @@
+patch_text = ""
+bug_text = ""
+graph_file = ""
+
+python3 action.py patch_text bug_text graph_file
+
+python3 apti_pattern.py patch_text bug_text graph_file
+
+python3 distancetopatch.py graph_file
+
+python3 editDistance.py patch_text bug_text graph_file
+
+python3 entropy.py patch_text graph_file
+
+python3 operator.py patch_text graph_file
+
+python3 statement.py patch_text graph_file
