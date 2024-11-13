44a26f4624f693a1f933f0a253d64e9e72761b7c
SEdeepL <153978170+SEdeepL@users.noreply.github.com>
2024-11-13 15:56:13 +0800

    Update layer.py

--- peft/tuners/lora/layer.py
+++ peft/tuners/lora/layer.py
@@ -562,7 +562,7 @@ class Linear(nn.Module, LoraLayer):
     def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
         self._check_forward_args(x, *args, **kwargs)
         adapter_names = kwargs.pop("adapter_names", None)
-        ipdb.set_trace()
+        #ipdb.set_trace()
 
         if self.disable_adapters:
             if self.merged:
